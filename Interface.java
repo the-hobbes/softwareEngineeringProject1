aa1e3ca322c5537f4df75f8216600102cb64e1a2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-01-21 10:54:39 -0800

    Setting up prototypes for functions in Interface

--- Interface.java
+++ Interface.java
@@ -5,5 +5,39 @@
 */
 
 public class Interface{
+
+	Deck theDeck = new Deck()
 	
+	static final PLAYER   = 0;
+	static final COMPUTER = 1;
+
+	Hand [] players = new Hand[2];
+
+	int currentTurn = 0;
+
+	public Interface(){
+		
+	}
+
+	public void setUpGame(){
+
+	}
+	
+	/**
+	*Displays the current person hand
+	*/
+	public void displayHand(){
+
+	}
+
+	/**
+	*Get the user input and handle it
+	*@return Returns the card the user asked for
+	*/
+	public int getCommand(){
+
+	}
+
+
+
 }
