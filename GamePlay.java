f0d13eff39d6fdfd4226f62011912547378452cb
Scott MacEwan <wsmacewan101@gmail.com>
2013-02-02 15:40:19 -0500

    Made sure game printed out score properly at end of game

--- GamePlay.java
+++ GamePlay.java
@@ -30,12 +30,12 @@ public class GamePlay{
 				}
 				goFish.setDeck(tempDeck);				
 			}	
-
+			goFish.endGame();
 			continueGoFish = UserInterface.getMenuOption();		
 
 		}
 		
-		goFish.endGame();
+		
 
 
 	}//main function
